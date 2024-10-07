77bebd577eeb79936aa1497720351a2daf197b45
jayden <jeonghoochoe@gmail.com>
2024-10-08 00:36:10 +0100

    [1207]Unique Number of Occurrences

--- src/main/java/leetcode/editor/en/UniqueNumberOfOccurrences.java
+++ src/main/java/leetcode/editor/en/UniqueNumberOfOccurrences.java
@@ -5,34 +5,33 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class UniqueNumberOfOccurrences{
+public class UniqueNumberOfOccurrences {
     public static void main(String[] args) {
-         Solution solution = new UniqueNumberOfOccurrences().new Solution();
+        Solution solution = new UniqueNumberOfOccurrences().new Solution();
     }
+    // [1207]Unique Number of Occurrences
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> record = new HashMap<>();
-        for (int i : arr){
-            if (record.containsKey(i)){
-                record.put(i, record.get(i) + 1);
-            } else {
-                record.put(i, 1);
-            }
-        }
+    class Solution {
+        public boolean uniqueOccurrences(int[] arr) {
+            Map<Integer, Integer> map = new HashMap<>();
 
-        Set<Integer> occur = new HashSet<>();
-        for (int i : record.values()){
-            if(occur.contains(i)){
-                return false;
-            }else {
-                occur.add(i);
+            for (int i : arr) {
+                map.put(i, map.getOrDefault(i, 0) + 1);
             }
-        }
 
-        return true;
+            Set<Integer> set = new HashSet<>();
+            for (int i : map.values()) {
+                if (set.contains(i)) {
+                    return false;
+                } else {
+                    set.add(i);
+                }
+            }
+
+            return true;
+
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
