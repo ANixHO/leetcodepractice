9dd0789a03372b762380e66a62240eaf47a122e3
jayden <jeonghoochoe@gmail.com>
2024-10-09 13:03:07 +0100

    [649]Dota2 Senate

--- src/main/java/leetcode/editor/en/Dota2Senate.java
+++ src/main/java/leetcode/editor/en/Dota2Senate.java
@@ -14,77 +14,47 @@ class Solution {
         // best time complexity solution:
         return voting(senate.toCharArray(), 0);
     }
-    public String voting(char[] senates, int vote){
 
+    // it is a recursive method, one round for one recursion
+    // when there is no other type of chars, it wins
+    private String voting(char[] chars, int vote){
         int r = 0;
         int d = 0;
 
-        for (int i = 0; i < senates.length; i++) {
-            if(senates[i] == 'R'){
+        // variable int vote represents that
+        // when it is positive, it means R wins for now
+        // when it is negative, it means D wins for now
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == 'R') {
 
-                if (vote >= 0){
-                    r++;
+                if (vote >= 0) {
+                    r ++;
                 } else {
-                    senates[i] = '0';
+                    chars[i] = '0';
                 }
+
                 vote ++;
 
-            }else if (senates[i] == 'D'){
+            } else if (chars[i] == 'D') {
 
-                if (vote <=0){
-                    d ++;
-                }else {
-                    senates[i] = '0';
+                if (vote <= 0) {
+                    d --;
+                } else {
+                    chars[i] = '0';
                 }
+
                 vote --;
             }
         }
 
-        if (d !=0 && r == 0){
-            return "Dire";
-        }else if (d == 0 && r != 0){
+        if (r != 0 && d == 0){
             return "Radiant";
+        } else if (d != 0 && r == 0) {
+            return "Dire";
         } else {
-            return voting(senates, vote);
+            return voting(chars, vote);
         }
 
-//        char[] chars = senate.toCharArray();
-//        Queue<Character> que = new LinkedList<>();
-//
-//        for (char c : chars){
-//            que.add(c);
-//        }
-//
-//        int r = 0;
-//        int d = 0;
-//
-//        while (que.size() > 1 ){
-//            char c = que.poll();
-//            if (c == 'R' && d > 0){
-//                d--;
-//            }else if (c == 'R' && d == 0){
-//                r ++;
-//                que.add(c);
-//            }
-//
-//            if (c == 'D' && r > 0){
-//                r --;
-//            } else if (c == 'D' && r == 0){
-//                d ++;
-//                que.add(c);
-//            }
-//
-//            if (r == que.size() || d == que.size()){
-//                break;
-//            }
-//
-//        }
-//
-//        if (que.peek() == 'R'){
-//            return "Radiant";
-//        } else {
-//            return "Dire";
-//        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
