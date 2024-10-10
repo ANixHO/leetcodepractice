96663522a5a54d5b0345298c058dad97fb209cf6
jayden <jeonghoochoe@gmail.com>
2024-10-10 07:55:49 +0100

    [11]Container With Most Water

--- src/main/java/leetcode/editor/en/ContainerWithMostWater.java
+++ src/main/java/leetcode/editor/en/ContainerWithMostWater.java
@@ -6,21 +6,30 @@ public class ContainerWithMostWater{
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxArea(int[] height) {
-        int max = 0;
-        int leftB = 0;
-        int rightB = height.length - 1;
+        int h = 0;
+        int res = 0;
+        int left = 0;
+        int right = height.length - 1;
 
-        while(leftB < rightB){
-            int cur = (rightB - leftB) * (Math.min(height[leftB], height[rightB]));
-            max = Math.max(max, cur);
+        while (right > left ){
+            int width = right - left;
+            h = Math.min(height[left], height[right]);
+            int cur = width * h;
 
-            if(height[leftB] > height[rightB]){
-                rightB --;
-            }else{
-                leftB ++;
+            if (cur > res) res = cur;
+
+            while(right > left && h >= height[left]){
+                left ++;
+            }
+
+            while(right > left && h >= height[right]){
+                right--;
             }
         }
-        return max;
+
+        return res;
+
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
