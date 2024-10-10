4c46c54ddec1308e9af6202ada4faf96eeb79e37
jayden <jeonghoochoe@gmail.com>
2024-10-10 07:41:38 +0100

    [104]Maximum Depth of Binary Tree

--- src/main/java/leetcode/editor/en/MaximumDepthOfBinaryTree.java
+++ src/main/java/leetcode/editor/en/MaximumDepthOfBinaryTree.java
@@ -2,46 +2,47 @@ package leetcode.editor.en;
 
 import leetcode.editor.en.utils.TreeNode;
 
-public class MaximumDepthOfBinaryTree{
+public class MaximumDepthOfBinaryTree {
     public static void main(String[] args) {
-         Solution solution = new MaximumDepthOfBinaryTree().new Solution();
+        Solution solution = new MaximumDepthOfBinaryTree().new Solution();
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int maxDepth(TreeNode root) {
-        if(root == null){
-            return 0;
+// [104]Maximum Depth of Binary Tree
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public int maxDepth(TreeNode root) {
+
+            if (root == null) return 0;
+
+            int left = 0;
+            int right = 0;
+
+            if (root != null){
+                left = maxDepth(root.left);
+            }
+
+            if (root!= null){
+                right = maxDepth(root.right);
+            }
+
+            return Math.max(left, right) + 1;
         }
-
-        int left = 0;
-        int right = 0;
-
-        if(root.left != null){
-            left = maxDepth(root.left);
-        }
-
-        if(root.right != null){
-            right = maxDepth(root.right);
-        }
-
-        return Math.max(left, right) + 1;
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
