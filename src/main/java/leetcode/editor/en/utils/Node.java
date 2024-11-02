864f1952dfc721718f41fff0bff0fe072b6f0ba5
jayden <jeonghoochoe@gmail.com>
2024-11-02 18:38:01 +0000

    11/2

--- src/main/java/leetcode/editor/en/utils/Node.java
+++ src/main/java/leetcode/editor/en/utils/Node.java
@@ -2,20 +2,13 @@ package leetcode.editor.en.utils;
 
 public class Node {
     public int val;
-    public Node left;
-    public Node right;
     public Node next;
+    public Node random;
 
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
     }
 }
+
