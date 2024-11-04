3209edcbff05d9dd6e84d65238a6ccc08ed3e1ee
jayden <jeonghoochoe@gmail.com>
2024-11-04 13:32:30 +0000

    [133]Clone Graph

---
+++ src/main/java/leetcode/editor/en/No133_CloneGraph.java
@@ -0,0 +1,70 @@
+  package leetcode.editor.en;
+
+  import java.util.ArrayList;
+  import java.util.HashMap;
+  import java.util.List;
+  import java.util.Map;
+
+  public class No133_CloneGraph{
+      public static void main(String[] args) {
+           Solution solution = new CloneGraph().new Solution();
+      }
+      // [133]Clone Graph
+      //leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    private Map<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        if (map.containsKey(node)){
+            return map.get(node);
+        }
+
+        Node clonedNode = new Node(node.val);
+        map.put(node, clonedNode);
+
+        for (Node neighbor : node.neighbors){
+            clonedNode.neighbors.add(cloneGraph(neighbor));
+        }
+        return clonedNode;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+  }
