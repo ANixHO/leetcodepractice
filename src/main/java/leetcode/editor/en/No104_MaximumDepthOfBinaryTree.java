edfea9d2ccfe6a3c11ba2ead4de0a4c0337c7c68
jayden <jeonghoochoe@gmail.com>
2024-11-19 10:35:14 +0000

    [104]Maximum Depth of Binary Tree

---
+++ src/main/java/leetcode/editor/en/No104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,47 @@
+  package leetcode.editor.en;
+
+  import leetcode.editor.en.utils.TreeNode;
+
+  public class No104_MaximumDepthOfBinaryTree{
+      public static void main(String[] args) {
+           Solution solution = new No104_MaximumDepthOfBinaryTree().new Solution();
+      }
+/* 
+[104]Maximum Depth of Binary Tree
+*/
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = 0;
+        int right = 0;
+        if (root.left != null){
+            left = maxDepth(root.left);
+        }
+        if (root.right != null){
+            right = maxDepth(root.right);
+        }
+        return Math.max(left, right) + 1;
+
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
