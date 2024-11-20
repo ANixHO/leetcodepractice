eb43ced78ab82951d15d42dc69cfa42f59810f95
jayden <jeonghoochoe@gmail.com>
2024-11-20 14:09:17 +0000

    [100]Same Tree

---
+++ src/main/java/leetcode/editor/en/No100_SameTree.java
@@ -0,0 +1,37 @@
+  package leetcode.editor.en;
+
+  import leetcode.editor.en.utils.TreeNode;
+
+  public class No100_SameTree{
+      public static void main(String[] args) {
+           Solution solution = new No100_SameTree().new Solution();
+      }
+/* 
+[100]Same Tree
+*/
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null || q == null) return p == q;
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
