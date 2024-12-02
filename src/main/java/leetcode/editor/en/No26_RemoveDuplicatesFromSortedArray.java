a810addd20657f49083692750d46ff0cca172a62
jayden <jeonghoochoe@gmail.com>
2024-12-02 00:23:14 +0000

    [26]Remove Duplicates from Sorted Array

--- src/main/java/leetcode/editor/en/No26_RemoveDuplicatesFromSortedArray.java
+++ src/main/java/leetcode/editor/en/No26_RemoveDuplicatesFromSortedArray.java
@@ -12,17 +12,16 @@ public class No26_RemoveDuplicatesFromSortedArray {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int removeDuplicates(int[] nums) {
-            int k = 0;
-            int prev = -101;
+           int index = 0;
+           int prev = -101;
 
             for (int i = 0; i < nums.length; i++) {
-                if (nums[i] > prev) {
-                    nums[k++] = nums[i];
+                if (nums[i] > prev){
+                    nums[index++] = nums[i];
                     prev = nums[i];
                 }
             }
-
-            return k;
+            return index;
 
         }
     }
