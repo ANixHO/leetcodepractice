ca4f843fb7b529cbad42ce72fe1bd98ad7682ba2
jayden <jeonghoochoe@gmail.com>
2024-12-27 09:17:35 +0700

    [1]Two Sum

---
+++ src/main/java/leetcode/editor/en/No1_TwoSum.java
@@ -0,0 +1,32 @@
+  package leetcode.editor.en;
+
+  import java.util.HashMap;
+  import java.util.Map;
+
+  public class No1_TwoSum{
+      public static void main(String[] args) {
+           Solution solution = new No1_TwoSum().new Solution();
+      }
+/* 
+[1]Two Sum
+*/
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // key: int in nums
+        // value : the index of int
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int find = target-nums[i];
+            if (map.containsKey(find)){
+                return new int[]{i, map.get(find)};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
